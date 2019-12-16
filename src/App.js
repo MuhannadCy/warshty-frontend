c656c981b22865e0360faf08495d128ca8b3af82
wahhab1404 <a.wahhab1404@gmail.com>
2019-12-16 22:45:03 +0300

    Landing page with button toggle functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
-import './App.scss'
-import { Route } from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.scss";
+import { Route } from "react-router-dom";
 
-import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
-import Header from './header/Header'
-import SignUp from './auth/components/SignUp'
-import SignIn from './auth/components/SignIn'
-import SignOut from './auth/components/SignOut'
-import ChangePassword from './auth/components/ChangePassword'
-import AddCustomer from './auth/components/AddCustomer'
-import AlertDismissible from './auth/components/AlertDismissible'
-import Customers from './auth/components/Customers'
+import AuthenticatedRoute from "./auth/components/AuthenticatedRoute";
+import Header from "./header/Header";
+import SignUp from "./auth/components/SignUp";
+import SignIn from "./auth/components/SignIn";
+import SignOut from "./auth/components/SignOut";
+import ChangePassword from "./auth/components/ChangePassword";
+import AddCustomer from "./auth/components/AddCustomer";
+import AlertDismissible from "./auth/components/AlertDismissible";
+import Customers from "./auth/components/Customers";
+import LandingPage from "./LandingPage";
 
 class App extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
 
     this.state = {
       user: null,
@@ -22,70 +23,110 @@ class App extends Component {
       showSingUp: false,
       showSignIn: false,
       customersList: []
-    }
+    };
   }
 
-  setUser = user => this.setState({ user })
+  setUser = user => this.setState({ user });
 
-  setCustomerList = customers =>{
-    this.setState({customersList: customers})
-  }
-  clearUser = () => this.setState({ user: null })
+  setCustomerList = customers => {
+    this.setState({ customersList: customers });
+  };
+  clearUser = () => this.setState({ user: null });
 
   alert = (message, type) => {
-    this.setState({ alerts: [...this.state.alerts, { message, type }] })
-  }
-  showSignUp = (e) =>{
-    e.preventDefault()
+    this.setState({ alerts: [...this.state.alerts, { message, type }] });
+  };
+  showSignUp = e => {
+    e.preventDefault();
     this.setState({
       showSingUp: true,
-      showSignIn: false,})
-  }
-  showSignIn = (e) =>{
-    e.preventDefault()
+      showSignIn: false
+    });
+  };
+  showSignIn = e => {
+    e.preventDefault();
     this.setState({
       showSingUp: false,
-      showSignIn: true,})
-  }
-  render () {
-    const { alerts, user } = this.state
+      showSignIn: true
+    });
+  };
+  render() {
+    const { alerts, user } = this.state;
 
     return (
       <React.Fragment>
         <Header user={user} />
         {alerts.map((alert, index) => (
-          <AlertDismissible key={index} variant={alert.type} message={alert.message} />
+          <AlertDismissible
+            key={index}
+            variant={alert.type}
+            message={alert.message}
+          />
         ))}
         <main className="container">
-          <Route path='/sign-up' render={() => (
-            <SignUp alert={this.alert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn alert={this.alert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword alert={this.alert} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/add-customer' render={() => (
-            <AddCustomer alert={this.alert} user={user} customers ={this.state.customersList} setCustomerList = {this.setCustomerList}/>
-          )} />
+          <Route
+            path="/sign-up"
+            render={() => <SignUp alert={this.alert} setUser={this.setUser} />}
+          />
+          <Route
+            path="/landingpage"
+            render={() => (
+              <LandingPage alert={this.alert} setUser={this.setUser} />
+            )}
+          />
+          <Route
+            path="/sign-in"
+            render={() => <SignIn alert={this.alert} setUser={this.setUser} />}
+          />
+          <AuthenticatedRoute
+            user={user}
+            path="/sign-out"
+            render={() => (
+              <SignOut
+                alert={this.alert}
+                clearUser={this.clearUser}
+                user={user}
+              />
+            )}
+          />
+          <AuthenticatedRoute
+            user={user}
+            path="/change-password"
+            render={() => <ChangePassword alert={this.alert} user={user} />}
+          />
+          <AuthenticatedRoute
+            user={user}
+            path="/add-customer"
+            render={() => (
+              <AddCustomer
+                alert={this.alert}
+                user={user}
+                customers={this.state.customersList}
+                setCustomerList={this.setCustomerList}
+              />
+            )}
+          />
         </main>
         <div>
-        <AuthenticatedRoute user={user} path='/' render={() => (
-            <Customers user={user} customers ={this.state.customersList} setCustomerList = {this.setCustomerList}/>
-          )} />
-         {/* <input type="submit" value="Sign Up" onClick={this.showSignUp} />
+          <AuthenticatedRoute
+            user={user}
+            path="/"
+            render={() => (
+              <Customers
+                user={user}
+                customers={this.state.customersList}
+                setCustomerList={this.setCustomerList}
+              />
+            )}
+          />
+          {/* <input type="submit" value="Sign Up" onClick={this.showSignUp} />
          <input type="submit" value="Sing In" onClick={this.showSignIn} />
           { this.state.showSingUp ? <SignUp alert={this.alert} setUser={this.setUser} /> : null }
           { this.state.showSignIn ? <SignIn alert={this.alert} setUser={this.setUser} /> : null } */}
         </div>
       </React.Fragment>
-     
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/LandingPage.js
@@ -0,0 +1,51 @@
+//Abdulwahhab
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
+import Header from './header/Header'
+import SignUp from './auth/components/SignUp'
+import SignIn from './auth/components/SignIn'
+import SignOut from './auth/components/SignOut'
+import ChangePassword from './auth/components/ChangePassword'
+import AddCustomer from './auth/components/AddCustomer'
+import AlertDismissible from './auth/components/AlertDismissible'
+import Customers from './auth/components/Customers'
+import './LandingPage.scss'
+import warshaty2 from './Assets/Images/warshaty2.png'
+export default class LandingPage extends Component {
+    constructor(props){
+        super(props)
+        this.state = { showSignUp: false , showSignIn: false};
+        
+    }
+
+    toggleSignInDiv = () =>{
+        const{showSignIn} =this.state;
+        this.setState( {showSignIn: !showSignIn})
+    }
+    toggleSignUpDiv = () =>{
+        const{showSignUp} =this.state;
+        this.setState( {showSignUp: !showSignUp})
+    }
+    render() {
+        return (
+            <div class = "container">
+                <div class = "sign">
+                    <div class= "sign-in">
+                        {/* <SignIn /> */}
+                        {this.state.showSignIn &&  <SignIn />}
+                    </div>
+                    <div class = "logo">
+                        <button class="sign-button" onClick = {this.toggleSignInDiv}>Sign In</button>
+                       
+                        <button class="sign-button" onClick = {this.toggleSignUpDiv}>Sign Up</button>
+                    </div>
+                    <div class= "sign-up">
+                        {this.state.showSignUp && <SignUp />}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
